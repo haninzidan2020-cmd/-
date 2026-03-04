c19548f2aadbb6b1884b24877352c304c9707cb3
haninzidan2020-cmd <haninzidan2020@gmail.com>
2026-03-04 20:22:21 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite'; // الضيف الجديد اللي هيحل المشكلة
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(), // حطيناه هنا عشان يشغل الألوان والتصميم
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'صحتك بالدنيا', // اسم التطبيق الكامل
-        short_name: 'صحتك', // الاسم القصير اللي هيظهر تحت الأيقونة
+        name: 'صحتك بالدنيا',
+        short_name: 'صحتك',
         description: 'تطبيق ذكي لمتابعة الصحة، السكر، الضغط، واليوميات الغذائية',
-        theme_color: '#10b981', // لون شريط الموبايل من فوق (الزمردي بتاعنا)
+        theme_color: '#10b981',
         background_color: '#ffffff',
-        display: 'standalone', // السر هنا: ده اللي بيخليه يفتح شاشة كاملة بدون شريط المتصفح
+        display: 'standalone',
         orientation: 'portrait',
         icons: [
           {
